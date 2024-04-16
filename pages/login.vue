a4815aa602dfe9a5e5feaf6ab99afb4eb8d48992
GregoryToc <gregory.toccanier@ecole-isitech.com>
2024-04-16 14:23:28 +0200

    🔒 Update login form and add authentication logic
    Refactored the login form to use email instead of username. Added v-models for email and password inputs. Implemented a login method that sends a POST request with user credentials, handles responses accordingly, and displays error messages when needed.

--- pages/login.vue
+++ pages/login.vue
@@ -4,15 +4,58 @@
             <h1 class="text-lg font-bold">Login</h1>
             <p class="text-gray-500 mb-4">Enter your credentials to access your account.</p>
             <form class="flex flex-col gap-3">
-                <UFormGroup label="Username">
-                    <UInput type="text" color="primary" variant="outline" placeholder="Username" />
+                <UFormGroup label="Email">
+                    <UInput type="text" color="primary" variant="outline" placeholder="Email" v-model="email" />
                 </UFormGroup>
                 <UFormGroup label="Password">
-                    <UInput type="password" color="primary" variant="outline" placeholder="Password" />
+                    <UInput type="password" color="primary" variant="outline" placeholder="Password" v-model="password" />
                 </UFormGroup>
-                <UButton type="submit" class="self-end w-fit">Sign in</UButton>
+                <UButton @click="() => login()" class="self-end w-fit">Sign in</UButton>
             </form>
         </UCard>
     </div>
     <NuxtPage />
 </template>
+
+<script>
+export default {
+    data() {
+        return {
+            email: 'johndoe@gmail.com',
+            password: '123456',
+        }
+    },
+    methods: {
+        async login() {
+            try {
+                const response = await fetch('http://localhost:3000/api/auth/login', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({
+                        email: this.email,
+                        password: this.password
+                    })
+                });
+                const data = await response.json();
+                if (response.status === 200) {
+                    // Ajout au localStorage
+                    localStorage.setItem('banane', data);
+                    // Redirection
+                    this.$router.push('/home');
+                }
+                else {
+                    console.log("error")
+                    const toast = useToast()
+                    toast.add({ title: "Ce ne sont pas les bons codes, banane !" })
+                }
+
+            } catch (error) {
+                console.error('Error:', error);
+                // Handle error, e.g., display an error message to the user
+            }
+        }
+    }
+}
+</script>
