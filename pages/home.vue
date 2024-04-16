ac81d9626f1b0f9361c209dc80903031471d0cd6
GregoryToc <gregory.toccanier@ecole-isitech.com>
2024-04-16 15:41:06 +0200

    🔧 Refactor code structure and API calls
    - Updated variable references in the template
    - Implemented fetching tasks on component mount
    - Added error handling for fetch requests
    - Modified task checkbox behavior and deletion functionality

--- pages/home.vue
+++ pages/home.vue
@@ -3,7 +3,7 @@
         <div class="flex items-center justify-between bg-blue-700 py-2 px-10">
             <span>TODOLIST</span>
             <div class="flex gap-4 items-center">
-                Bonjour, {{ banane.email }}
+                Bonjour, {{ banane.name }}
                 <UButton color="white" @click="handleClickDisconnectButton" class="w-1/6 self-end w-fit">Se déconnecter
                 </UButton>
             </div>
@@ -15,9 +15,10 @@
             </div>
             <div class="flex flex-col gap-2 w-1/2 mt-5">
                 <div v-for="(task, index) in tasks" :key="index" class="flex items-center gap-2">
-                    <input type="checkbox" v-model="task.completed" />
+                    <input type="checkbox" :checked="task.status === 'completed'"
+                        @change="handleChangeCheckboxState(task)" />
                     <span>{{ task.name }}</span>
-                    <UButton color="red" @click="handleClickDeleteTaskButton(index)" class="w-fit">Delete</UButton>
+                    <UButton color="red" @click="handleClickDeleteTaskButton(task.id)" class="w-fit">Delete</UButton>
                 </div>
             </div>
         </div>
@@ -29,22 +30,111 @@ export default {
     data() {
         return {
             newTask: '',
-            banane: JSON.parse(localStorage.getItem('banane')),
+            banane: {},
             tasks: []
         };
     },
+    mounted() {
+        this.banane = JSON.parse(localStorage.getItem('banane'));
+        this.fetchTasks();
+    },
     methods: {
-        handleClickAddTaskButton() {
-            if (this.newTask.trim() !== '') {
-                this.tasks.push({
-                    name: this.newTask,
-                    completed: false
-                });
-                this.newTask = '';
+        async fetchTasks() {
+            try {
+                const response = await fetch('/api/users/' + this.banane.id + '/lists');
+                const data = await response.json();
+                if (response.status === 200) {
+                    console.log(data)
+                    this.tasks = data;
+                }
+                else {
+                    const toast = useToast()
+                    toast.add({ title: "Ça marche pas je crois !" })
+                }
+
+            } catch (error) {
+                console.error('Error:', error);
+                // Handle error, e.g., display an error message to the user
+                const toast = useToast()
+                toast.add({ title: "YA UNE ERREUR !" })
             }
         },
-        handleClickDeleteTaskButton(index) {
-            this.tasks.splice(index, 1);
+        async handleClickAddTaskButton() {
+            try {
+                const response = await fetch('/api/users/' + this.banane.id + '/lists', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({
+                        name: this.newTask,
+                    })
+                });
+                const data = await response.json();
+                if (response.status === 200) {
+                    this.tasks.push(data);
+                }
+                else {
+                    const toast = useToast()
+                    toast.add({ title: "Ça marche pas je crois !" })
+                }
+
+            } catch (error) {
+                console.error('Error:', error);
+                // Handle error, e.g., display an error message to the user
+                const toast = useToast()
+                toast.add({ title: "YA UNE ERREUR !" })
+            }
+        },
+        async handleChangeCheckboxState(task) {
+            try {
+                const response = await fetch('/api/users/' + this.banane.id + '/lists/' + task.id, {
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    method: 'PATCH',
+                    body: JSON.stringify({
+                        status: task.status === 'completed' ? 'active' : 'completed'
+                    })
+                });
+                if (response.status === 200) {
+                    console.log(this.fetchTasks())
+                    this.fetchTasks();
+                } else {
+                    console.log(response)
+                    const toast = useToast()
+                    toast.add({ title: "Ça marche pas je crois !" })
+                }
+
+            } catch (error) {
+                console.error('Error:', error);
+                // Handle error, e.g., display an error message to the user
+                const toast = useToast()
+                toast.add({ title: "YA UNE ERREUR !" })
+            }
+        },
+        async handleClickDeleteTaskButton(id) {
+            try {
+                const response = await fetch('/api/users/' + this.banane.id + '/lists/' + id, {
+                    method: 'DELETE',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                });
+                if (response.status === 204) {
+                    this.tasks = this.tasks.filter(t => t.id !== id);
+                }
+                else {
+                    const toast = useToast()
+                    toast.add({ title: "Ça marche pas je crois !" })
+                }
+
+            } catch (error) {
+                console.error('Error:', error);
+                // Handle error, e.g., display an error message to the user
+                const toast = useToast()
+                toast.add({ title: "YA UNE ERREUR !" })
+            }
         },
         handleClickDisconnectButton() {
             localStorage.removeItem('banane');
--- pages/login.vue
+++ pages/login.vue
@@ -28,7 +28,7 @@ export default {
     methods: {
         async login() {
             try {
-                const response = await fetch('http://localhost:3000/api/auth/login', {
+                const response = await fetch('/api/auth/login', {
                     method: 'POST',
                     headers: {
                         'Content-Type': 'application/json'
@@ -54,6 +54,8 @@ export default {
             } catch (error) {
                 console.error('Error:', error);
                 // Handle error, e.g., display an error message to the user
+                const toast = useToast()
+                toast.add({ title: "YA UNE ERREUR !" })
             }
         }
     }
